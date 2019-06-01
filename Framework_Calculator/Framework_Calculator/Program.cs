ab6b0d708d328935cfdb497bc72fe374d717826d
unknown <xingkunyin.hotmail.com>
2019-06-01 19:04:56 -0400

    Get the winform part reday.

---
+++ Framework_Calculator/Framework_Calculator/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Calculator
+{
+    public partial class Program : Form
+    {
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+
+        public Program()
+        {
+            InitializeComponent();
+        }
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Program());
+            // Declare variables and then initialize to zero
+            int num1 = 0; int num2 = 0;
+
+            // Display title as the C# console calculator app
+            // Display title as the C# console calculator app
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to type the first number
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to type the second number
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to choose an option
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            // Use a switch statement to do the math
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            // Wait for the user to respond before closing
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(202, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 88);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(338, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Subtract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(202, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 88);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Multiply";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(338, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 88);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Devide";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 36);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Num1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(169, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 36);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Num2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Program
+            // 
+            this.ClientSize = new System.Drawing.Size(680, 586);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Program";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("you clicked Me", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
